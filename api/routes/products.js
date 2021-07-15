65cafd09bf1a6abdec30417f902099f678a72b53
BENOIT-HAVET LEE <myungjae84@gmail.com>
2021-07-16 00:35:42 +0200

    validation Mongoose and request object

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,11 +5,27 @@ const mongoose = require("mongoose");
 
 router.get("/", (req, res, next) => {
   Product.find()
+    // wanna only this 3 property only
+    .select("name price _id")
     .exec()
     .then((docs) => {
-      console.log(docs);
       if (docs.length > 0) {
-        res.status(200).json(docs);
+        // set a new format of response
+        const response = {
+          count: docs.length,
+          products: docs.map((doc) => {
+            return {
+              name: doc.name,
+              price: doc.price,
+              _id: doc._id,
+              request: {
+                type: "GET",
+                url: "http://localhost:3000/products/" + doc._id,
+              },
+            };
+          }),
+        };
+        res.status(200).json(response);
       } else {
         res.status(404).json({ message: "No entry found" });
       }
@@ -28,31 +44,58 @@ router.post("/", (req, res, next) => {
     price: req.body.price,
   });
   //persist in database my new instance created
-  product
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.status(201).json({
-        messages: "Handling POST resquest to /products",
-        createdProduct: product,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
+  const productExist = Product.findOne({
+    existingName: req.body.name,
+  });
+  if (productExist) {
+    res.status(406).json({
+      error: { message: "This product name already exists in the list" },
     });
+  } else {
+    product
+      .save()
+      .then((result) => {
+        console.log(result);
+        res.status(201).json({
+          messages: "Created product successfully",
+          createdProduct: {
+            name: result.name,
+            price: result.price,
+            _id: result._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + result._id,
+            },
+          },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
+      });
+  }
 });
 
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select("name price _id")
     .exec()
     .then((doc) => {
-      console.log("from data base", doc);
       if (doc) {
-        res.status(200).json(doc);
+        const response = {
+          name: doc.name,
+          price: doc.price,
+          _id: doc._id,
+          request: {
+            type: "GET",
+            description: "get all products",
+            url: "http://localhost:3000/products/",
+          },
+        };
+        res.status(200).json(response);
       } else {
         res.status(404).json({ message: "No valid id found" });
       }
@@ -70,12 +113,16 @@ router.patch("/:productId", (req, res, next) => {
   for (const ops of req.body) {
     updateOps[ops.propName] = ops.value;
   }
-  console.log("updateOps", updateOps);
   Product.updateOne({ _id: id }, { $set: updateOps })
     .exec()
     .then((result) => {
-      console.log(result);
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product updated successfully",
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/products/" + id,
+        },
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -87,7 +134,16 @@ router.delete("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.remove({ _id: id })
     .exec()
-    .then((result) => res.status(200).json(result))
+    .then((result) =>
+      res.status(200).json({
+        message: "Deleted successfully",
+        request: {
+          type: "POST",
+          url: "http://localhost:3000/products/",
+          body: { name: "String", price: "Number" },
+        },
+      })
+    )
     .catch((err) => {
       console.log(err);
       res.status(500).json({ error: err });
