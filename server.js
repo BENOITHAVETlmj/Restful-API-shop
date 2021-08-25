ba7ba78ccfbe623f47620e29a1f460fd208f5902
BENOIT-HAVET LEE <myungjae84@gmail.com>
2021-08-25 22:18:17 +0200

    port process env

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const http = require("http");
-const PORT = process.env.PORT || 3000;
 const app = require("./app");
 console.log(process.env);
 const server = http.createServer(app);
-server.listen(PORT, () => {
+server.listen(process.env.port, () => {
   console.log(`server started on ${PORT}`);
 });
