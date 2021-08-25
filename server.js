ec4770e8246c90db3d93b0a1c7748bf4b8efc17e
BENOIT-HAVET LEE <myungjae84@gmail.com>
2021-08-25 16:51:41 +0200

    server listen method fix

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ const PORT = process.env.SERVER_APP || 3000;
 const app = require("./app");
 console.log(process.env);
 const server = http.createServer(app);
-server.listen(PORT, () => console.log(`server started on ${PORT}`));
+server.listen(PORT, () => {
+  console.log(`server started on ${PORT}`);
+});
